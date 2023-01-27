e1694eaec02bfa296d00a5f0ba15829bc541e29f
nu09 <nurihaji911@gmail.com>
2023-01-27 04:00:09 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: no return.
  */
 
-void __attribute__ ((constructor)) bmain()
+void __attribute__ ((constructor)) bmain(void)
 {
 	printf("you're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
